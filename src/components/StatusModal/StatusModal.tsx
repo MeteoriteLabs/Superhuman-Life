690c2e94979ec00cd589d4166c2f08587f54ce34
Vikas Ravindran <vikas.ravindran@compugain.com>
2021-07-20 14:32:12 +0530

    added display trigger for confirmation modal

--- src/components/StatusModal/StatusModal.tsx
+++ src/components/StatusModal/StatusModal.tsx
@@ -1,40 +1,44 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import { Button, Col, Modal, Row } from "react-bootstrap";
 
 
-function StatusModal(props: any) {
-    const [show, setShow] = useState<boolean>(true);
-    function handleClick(){
-        props.onClick();
-        setShow(false);
-    }
+function StatusModal({ modalTitle, modalBody, buttonLeft, buttonRight, modalTrigger, onClick }: any) {
+  const [show, setShow] = useState<boolean>(false);
+
+  function handleClick() {
+    onClick();
+    setShow(false);
+  }
+
+  modalTrigger.subscribe((res: boolean) => {
+    setShow(res);
+  });
+
   return (
     <>
       <Modal show={show} onHide={() => setShow(false)} aria-labelledby="contained-modal-title-vcenter" centered>
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-vcenter">
-            {props.modalTitle}
+            {modalTitle}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body className="show-grid">
           <div>
-          <h5>{props.modalBody}</h5 >
-          </div>    
-        </Modal.Body>   
+            <h5>{modalBody}</h5 >
+          </div>
+        </Modal.Body>
         <Modal.Footer>
-        <div>
+          <div>
             <Row>
               <Col xs={4} md={4} className="ml-4">
-              <Button  variant="danger" onClick={() => setShow(false)}>{props.buttonLeft}</Button>
+                <Button variant="danger" onClick={() => setShow(false)}>{buttonLeft}</Button>
               </Col>
               <Col xs={4} md={5} className="ml-4">
-              <Button  variant="success" onClick={handleClick}>{props.buttonRight}</Button>
+                <Button variant="success" onClick={handleClick}>{buttonRight}</Button>
               </Col>
             </Row>
           </div>
-        </Modal.Footer> 
-          
-        
+        </Modal.Footer>
       </Modal>
     </>
   );
