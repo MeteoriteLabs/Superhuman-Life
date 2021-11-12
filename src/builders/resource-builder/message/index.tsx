95d3aaf49bebe37b4108988b34ef0faee72ee6e8
shivambalwani <shivambalwani2001@gmail.com>
2021-11-12 15:31:44 +0530

    fixed actionbutton for resources

--- src/builders/resource-builder/message/index.tsx
+++ src/builders/resource-builder/message/index.tsx
@@ -1,107 +1,145 @@
-import {useMemo,useRef,useState,useContext} from 'react'
-import {Badge,Button,TabContent,InputGroup,FormControl,Card,Container,Row,Col} from "react-bootstrap";
+import { useMemo, useRef, useState, useContext } from "react";
+import { Badge, Button, TabContent, InputGroup, FormControl, Card, Container, Row, Col } from "react-bootstrap";
 import Table from "../../../components/table";
-import {useQuery} from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import AuthContext from "../../../context/auth-context";
 import CreateEditMessage from "./createoredit-message";
 import ActionButton from "../../../components/actionbutton/index";
-import { GET_MESSAGES} from "./queries";
-
+import { GET_MESSAGES } from "./queries";
 
 export default function MindsetPage() {
-    const [searchFilter, setSearchFilter] = useState('');
-    const searchInput = useRef<any>();
-    const auth = useContext(AuthContext);
-    const createEditMessageComponent = useRef<any>(null);
-    
-    const columns = useMemo<any>(() => [
-        { accessor: "title", Header: "Title" },
-        {
-            accessor: "type",Header: "Type",
-        },
-        { accessor: "tags", Header: "Tags" },
-        { accessor: "minidesc", Header: "Mini Description" },
-        { accessor: "status", Header: "Status", Cell: (v: any) => <Badge variant={v.value === "Active" ? "success" : "danger"}>{v.value}</Badge> },
-        { accessor: "updatedon", Header: "Updated On" },
-        
-        {
-            id: "edit",
-            Header: "Actions",
-            Cell: ({ row }: any) => (
-                <ActionButton 
-                action1="Edit"
-                actionClick1={() => {createEditMessageComponent.current.TriggerForm({id: row.original.id, type: 'edit'})}}
-                action2="View"
-                actionClick2={() => {createEditMessageComponent.current.TriggerForm({id: row.original.id, type: 'view'})}}
-                action3="Status"
-                actionClick3={() => { createEditMessageComponent.current.TriggerForm({ id: row.original.id, type: 'toggle-status', current_status: (row.original.status === "Active") }) }}
-                action4="Delete"
-                actionClick4={() => {createEditMessageComponent.current.TriggerForm({id: row.original.id, type: 'delete'})}}
-                 />
-            ),
-        }
-    ], []);
+     const [searchFilter, setSearchFilter] = useState("");
+     const searchInput = useRef<any>();
+     const auth = useContext(AuthContext);
+     const createEditMessageComponent = useRef<any>(null);
 
+     const columns = useMemo<any>(
+          () => [
+               { accessor: "title", Header: "Title" },
+               {
+                    accessor: "type",
+                    Header: "Type",
+               },
+               { accessor: "tags", Header: "Tags" },
+               { accessor: "minidesc", Header: "Mini Description" },
+               {
+                    accessor: "status",
+                    Header: "Status",
+                    Cell: (v: any) => <Badge variant={v.value === "Active" ? "success" : "danger"}>{v.value}</Badge>,
+               },
+               { accessor: "updatedon", Header: "Updated On" },
+               {
+                    id: "edit",
+                    Header: "Actions",
+                    Cell: ({ row }: any) => {
+                         const actionClick1 = () => {
+                              createEditMessageComponent.current.TriggerForm({ id: row.original.id, type: "edit" });
+                         };
+                         const actionClick2 = () => {
+                              createEditMessageComponent.current.TriggerForm({ id: row.original.id, type: "view" });
+                         };
+                         const actionClick3 = () => {
+                              createEditMessageComponent.current.TriggerForm({
+                                   id: row.original.id,
+                                   type: "toggle-status",
+                                   current_status: row.original.status === "Active",
+                              });
+                         };
+                         const actionClick4 = () => {
+                              createEditMessageComponent.current.TriggerForm({ id: row.original.id, type: "delete" });
+                         };
 
-    function getDate(time: any) {
-        let dateObj = new Date(time);
-        let month = dateObj.getMonth()+1;
-        let year = dateObj.getFullYear();
-        let date = dateObj.getDate();
+                         const arrayAction = [
+                              { actionName: "Edit", actionClick: actionClick1 },
+                              { actionName: "View", actionClick: actionClick2 },
+                              { actionName: "Status", actionClick: actionClick3 },
+                              { actionName: "Delete", actionClick: actionClick4 },
+                         ];
 
-      return(`${date}/${month}/${year}`);
-    }
-    
-    const [datatable, setDataTable] = useState<{}[]>([]);
+                         return <ActionButton arrayAction={arrayAction}></ActionButton>;
+                    },
+               },
+          ],
+          []
+     );
 
-    function FetchData(_variables: {} = { filter: " " ,id : auth.userid }) {
-        useQuery(GET_MESSAGES, { variables: _variables, onCompleted: loadData })
-    }
+     function getDate(time: any) {
+          let dateObj = new Date(time);
+          let month = dateObj.getMonth() + 1;
+          let year = dateObj.getFullYear();
+          let date = dateObj.getDate();
 
-    function loadData(data: any) {
-        setDataTable(
-            [...data.mindsetmessages].map((Detail) => {
-                return {
-                id: Detail.id,    
-                title : Detail.title,
-                tags : Detail.tags,
-                type: Detail.mindsetmessagetype.type,
-                minidesc: Detail.minidescription,
-                status: Detail.status ? "Active" : "Inactive",
-                updatedon: getDate(Date.parse(Detail.updatedAt))
-                }
-            })
-        );
-    }
+          return `${date}/${month}/${year}`;
+     }
 
-    FetchData({ filter: searchFilter, id: auth.userid});
-    return (
-        <TabContent>
-            <Container>
-            <Row>   
-            <Col>     
-            <InputGroup className="mb-3" >
-                <FormControl aria-describedby="basic-addon1" placeholder="Search"  ref={searchInput}/>
-                    <InputGroup.Prepend>
-                        <Button variant="outline-secondary" onClick={(e:any) => {e.preventDefault(); setSearchFilter(searchInput.current.value)}} ><i className="fas fa-search"></i></Button>
-                    </InputGroup.Prepend>
-            </InputGroup>
-            </Col>
-            <Col>  
-            <Card.Title className="text-center">
-            <Button variant={true ? "outline-secondary" : "light"} size="sm"
-                                onClick={() => {
-                                    createEditMessageComponent.current.TriggerForm({ id: null, type: 'create',modal_status: true });
-                                }}
-                            >
-                                <i className="fas fa-plus-circle"></i>{" "}Create New
-                            </Button>
-                            <CreateEditMessage ref={createEditMessageComponent}></CreateEditMessage>
-            </Card.Title>
-            </Col>
-            </Row>
-            </Container>
-            <Table columns={columns} data={datatable} />
-        </TabContent>
-    );
+     const [datatable, setDataTable] = useState<{}[]>([]);
+
+     function FetchData(_variables: {} = { filter: " ", id: auth.userid }) {
+          useQuery(GET_MESSAGES, { variables: _variables, onCompleted: loadData });
+     }
+
+     function loadData(data: any) {
+          setDataTable(
+               [...data.mindsetmessages].map((Detail) => {
+                    return {
+                         id: Detail.id,
+                         title: Detail.title,
+                         tags: Detail.tags,
+                         type: Detail.mindsetmessagetype.type,
+                         minidesc: Detail.minidescription,
+                         status: Detail.status ? "Active" : "Inactive",
+                         updatedon: getDate(Date.parse(Detail.updatedAt)),
+                    };
+               })
+          );
+     }
+
+     FetchData({ filter: searchFilter, id: auth.userid });
+     return (
+          <TabContent>
+               <Container>
+                    <Row>
+                         <Col>
+                              <InputGroup className="mb-3">
+                                   <FormControl
+                                        aria-describedby="basic-addon1"
+                                        placeholder="Search"
+                                        ref={searchInput}
+                                   />
+                                   <InputGroup.Prepend>
+                                        <Button
+                                             variant="outline-secondary"
+                                             onClick={(e: any) => {
+                                                  e.preventDefault();
+                                                  setSearchFilter(searchInput.current.value);
+                                             }}
+                                        >
+                                             <i className="fas fa-search"></i>
+                                        </Button>
+                                   </InputGroup.Prepend>
+                              </InputGroup>
+                         </Col>
+                         <Col>
+                              <Card.Title className="text-center">
+                                   <Button
+                                        variant={true ? "outline-secondary" : "light"}
+                                        size="sm"
+                                        onClick={() => {
+                                             createEditMessageComponent.current.TriggerForm({
+                                                  id: null,
+                                                  type: "create",
+                                                  modal_status: true,
+                                             });
+                                        }}
+                                   >
+                                        <i className="fas fa-plus-circle"></i> Create New
+                                   </Button>
+                                   <CreateEditMessage ref={createEditMessageComponent}></CreateEditMessage>
+                              </Card.Title>
+                         </Col>
+                    </Row>
+               </Container>
+               <Table columns={columns} data={datatable} />
+          </TabContent>
+     );
 }
