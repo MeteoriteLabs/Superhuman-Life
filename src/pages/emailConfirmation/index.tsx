f2cd3db0b52f2a440e94cacfd3ca1afb3c2e6013
itzVort3x1 <saikaustubh10@gmail.com>
2022-03-23 12:05:34 +0530

    updated email confirmation

--- src/pages/emailConfirmation/index.tsx
+++ src/pages/emailConfirmation/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Modal, Row } from "react-bootstrap";
 import { useMutation, gql } from '@apollo/client';
 import { useLocation } from "react-router-dom";
@@ -8,6 +8,7 @@ export default function ForgotPassword() {
     let query = new URLSearchParams(useLocation().search);
     const confirmation = query.get("confirmation");
     const [emailSent, setEmailSent] = useState(false);
+    const [errorVerification, setErrorVerification] = useState(false);
 
     const CONFIRM_ACCOUNT = gql`
       mutation accountConfirmation($confirmation: String!) {
@@ -17,15 +18,17 @@ export default function ForgotPassword() {
       }
     `;
 
-    const [confirmAccount] = useMutation(CONFIRM_ACCOUNT, { onCompleted: () => { setEmailSent(true) } });
+    const [confirmAccount] = useMutation(CONFIRM_ACCOUNT, { onCompleted: () => { setEmailSent(true) }, onError: () => {setErrorVerification(true)} });
+
+    useEffect(() => {
+      confirmAccount({ variables: {
+        confirmation: confirmation
+      }});
+    }, []);
 
   return (
-    <Modal.Dialog>
-      {/* <Helmet>
-        <meta charSet="utf-8" />
-        <title>Sapien Dashboard | Login</title>
-        <link rel="canonical" href="https://sapien.systems/" />
-      </Helmet> */}
+    <>
+    {emailSent && <Modal.Dialog>
       <Modal.Body>
       {emailSent && 
             <div>
@@ -34,7 +37,6 @@ export default function ForgotPassword() {
                 </Row>
                 <blockquote className="blockquote text-right">
                     <p className="text-danger blockquote-footer">Do not share your password with others</p>
-                    {/* <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer> */}
                 </blockquote>
                 <Row style={{ justifyContent: 'center', justifyItems: 'center'}}>
                     <Button onClick={() => window.location.href = '/login'} size="sm" variant="danger">
@@ -43,19 +45,21 @@ export default function ForgotPassword() {
                 </Row>
             </div>
         }
-        {!emailSent && <><h4>Email Verification</h4>
-        <p className="text-danger blockquote-footer">Verify to use the application</p>
-        <hr />
-
-          <Row className="mb-2" style={{justifyContent: 'center'}}>
-            <Button onClick={() => { confirmAccount({ variables: {
-                confirmation: confirmation
-            }})}} type="submit" size="sm" variant="danger">
-                Verify Email
-            </Button>
-          </Row>
-         </>}
       </Modal.Body>
-    </Modal.Dialog>
+    </Modal.Dialog>}
+
+    {errorVerification && <Modal.Dialog>
+      <Modal.Body>
+        <div>
+            <Row style={{ justifyContent: 'center', justifyItems: 'center'}}>
+                <h1 style={{color: 'red'}}><img src="/assets/confirmed.svg" alt="confirmed"></img>Email Not Verified</h1>
+            </Row>
+            <blockquote className="blockquote text-right">
+                <p className="text-danger blockquote-footer">Link Not Valid</p>
+            </blockquote>
+        </div>
+      </Modal.Body>
+    </Modal.Dialog>}
+    </>
   );
 }
