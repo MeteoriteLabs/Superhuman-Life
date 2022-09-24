6829ce198f70e2154c9661892f7c1e8736ebaa8b
itzVort3x1 <saikaustubh10@gmail.com>
2022-09-24 10:18:01 +0530

    removed conosle warnings

--- src/builders/package-builder/fitness/group/groupConfig.tsx
+++ src/builders/package-builder/fitness/group/groupConfig.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {useState } from 'react';
 import { Form, Row, Col } from 'react-bootstrap';
 
 const PackageDateConfig = (props: any) => {
