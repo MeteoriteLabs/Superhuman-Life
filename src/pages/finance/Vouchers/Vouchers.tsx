ee70034bd4bd5d561898a238764b4f83a67f864c
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2023-05-23 12:36:38 +0530

    feat: vouchers page ts

--- src/pages/finance/Vouchers/Vouchers.tsx
+++ src/pages/finance/Vouchers/Vouchers.tsx
@@ -1,22 +1,40 @@
-import React from "react";
-import { Badge, Row, Col, Button } from "react-bootstrap";
-import { useContext, useMemo, useRef, useState } from "react";
-import Table from "../../../components/table/index";
-import ActionButton from "../../../components/actionbutton";
-import { useQuery } from "@apollo/client";
-import { GET_ALL_VOUCHERS } from "../graphQL/queries";
-import authContext from "../../../context/auth-context";
-import moment from "moment";
-import VoucherAction from "./VoucherAction";
+import React from 'react';
+import { Badge, Row, Col, Button } from 'react-bootstrap';
+import { useContext, useMemo, useRef, useState } from 'react';
+import Table from '../../../components/table/index';
+import ActionButton from '../../../components/actionbutton';
+import { useQuery } from '@apollo/client';
+import { GET_ALL_VOUCHERS } from '../graphQL/queries';
+import authContext from '../../../context/auth-context';
+import moment from 'moment';
+import VoucherAction from './VoucherAction';
 
-export default function Vouchers() {
+interface VoucherTs {
+  TriggerForm: ({
+    id,
+    actionType,
+    current_status
+  }: {
+    id?: string | number;
+    actionType: string;
+    current_status?: string;
+  }) => void;
+}
+interface RowTs {
+  row: {
+    values: { discount_percentage?: string; expiry_date?: string; Status?: string };
+    original: { id: string; Status: string };
+  };
+}
+
+export default function Vouchers(): JSX.Element {
   const auth = useContext(authContext);
-  const [dataTable, setDataTable] = useState<any[]>([]);
-  const voucherActionRef = useRef<any>(null);
+  const [dataTable, setDataTable] = useState<Record<string, unknown>[]>([]);
+  const voucherActionRef = useRef<VoucherTs>(null);
 
   const fetch = useQuery(GET_ALL_VOUCHERS, {
     variables: { id: auth.userid },
-    onCompleted: (data) => loadData(data),
+    onCompleted: (data) => loadData(data)
   });
 
   const loadData = (data) => {
@@ -29,14 +47,12 @@ export default function Vouchers() {
           id: voucher.id,
           voucher_name: voucher.attributes.voucher_name,
           discount_percentage: voucher.attributes.discount_percentage,
-          expiry_date: moment(voucher.attributes.expiry_date).format(
-            "MMMM DD,YYYY"
-          ),
+          expiry_date: moment(voucher.attributes.expiry_date).format('MMMM DD,YYYY'),
           Usage_restriction: voucher.attributes.Usage_restriction,
           Status:
             diff <= 0 || voucher.attributes.Usage_restriction <= 0
-              ? "Expired"
-              : voucher.attributes.Status,
+              ? 'Expired'
+              : voucher.attributes.Status
         };
       })
     );
@@ -48,92 +64,91 @@ export default function Vouchers() {
 
   const columns = useMemo(
     () => [
-      { accessor: "voucher_name", Header: "Code" },
+      { accessor: 'voucher_name', Header: 'Code' },
       {
-        accessor: "discount_percentage",
-        Header: "Discount",
-        Cell: ({ row }: any) => {
+        accessor: 'discount_percentage',
+        Header: 'Discount',
+        Cell: ({ row }: RowTs) => {
           return <p className="mb-0">{row.values.discount_percentage} %</p>;
-        },
+        }
       },
       {
-        accessor: "expiry_date",
-        Header: "Expiry",
-        Cell: ({ row }: any) => {
+        accessor: 'expiry_date',
+        Header: 'Expiry',
+        Cell: ({ row }: RowTs) => {
           return <p className="mb-0">{row.values.expiry_date}</p>;
-        },
+        }
       },
-      { accessor: "Usage_restriction", Header: "Usage" },
+      { accessor: 'Usage_restriction', Header: 'Usage' },
       {
-        accessor: "Status",
-        Header: "Status",
-        Cell: ({ row }: any) => {
-          let statusColor = "";
+        accessor: 'Status',
+        Header: 'Status',
+        Cell: ({ row }: RowTs) => {
+          let statusColor = '';
           switch (row.values.Status) {
-            case "Active":
-              statusColor = "success";
+            case 'Active':
+              statusColor = 'success';
               break;
 
-            case "Expired":
-              statusColor = "danger";
+            case 'Expired':
+              statusColor = 'danger';
               break;
 
-            case "Disabled":
-              statusColor = "warning";
+            case 'Disabled':
+              statusColor = 'warning';
               break;
           }
           return (
             <>
               <Badge
                 className="px-3 py-1"
-                style={{ fontSize: "1rem", borderRadius: "10px" }}
-                variant={statusColor}
-              >
+                style={{ fontSize: '1rem', borderRadius: '10px' }}
+                variant={statusColor}>
                 {row.values.Status}
               </Badge>
             </>
           );
-        },
+        }
       },
       {
-        id: "edit",
-        Header: "Actions",
-        Cell: ({ row }: any) => {
+        id: 'edit',
+        Header: 'Actions',
+        Cell: ({ row }: RowTs) => {
           const viewHandler = () => {
-            voucherActionRef.current.TriggerForm({
+            voucherActionRef.current?.TriggerForm({
               id: row.original.id,
-              actionType: "view",
+              actionType: 'view'
             });
           };
           const editHandler = () => {
-            voucherActionRef.current.TriggerForm({
+            voucherActionRef.current?.TriggerForm({
               id: row.original.id,
-              actionType: "edit",
+              actionType: 'edit'
             });
           };
           const deleteHandler = () => {
-            voucherActionRef.current.TriggerForm({
+            voucherActionRef.current?.TriggerForm({
               id: row.original.id,
-              actionType: "delete",
+              actionType: 'delete'
             });
           };
           const statusChangeHandler = () => {
-            voucherActionRef.current.TriggerForm({
+            voucherActionRef.current?.TriggerForm({
               id: row.original.id,
-              actionType: "toggle-status",
-              current_status: row.original.Status,
+              actionType: 'toggle-status',
+              current_status: row.original.Status
             });
           };
           const arrayAction = [
-            { actionName: "View", actionClick: viewHandler },
-            { actionName: "Edit", actionClick: editHandler },
-            { actionName: "Delete", actionClick: deleteHandler },
-            { actionName: "Status", actionClick: statusChangeHandler },
+            { actionName: 'View', actionClick: viewHandler },
+            { actionName: 'Edit', actionClick: editHandler },
+            { actionName: 'Delete', actionClick: deleteHandler },
+            { actionName: 'Status', actionClick: statusChangeHandler }
           ];
 
           return <ActionButton arrayAction={arrayAction}></ActionButton>;
-        },
-      },
+        }
+      }
     ],
     []
   );
@@ -145,19 +160,15 @@ export default function Vouchers() {
           variant="outline-secondary"
           size="sm"
           onClick={() => {
-            voucherActionRef.current.TriggerForm({ actionType: "create" });
-          }}
-        >
+            voucherActionRef.current?.TriggerForm({ actionType: 'create' });
+          }}>
           <i className="fas fa-plus-circle"></i> Create Voucher
         </Button>
       </div>
       <Row>
         <Col>
           <Table columns={columns} data={dataTable} />
-          <VoucherAction
-            ref={voucherActionRef}
-            callback={refetchQueryCallback}
-          />
+          <VoucherAction ref={voucherActionRef} callback={refetchQueryCallback} />
         </Col>
       </Row>
     </div>
