699b9b3cf794fb98b2a8dd66959b1e330ec6749a
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2023-05-29 15:40:40 +0530

    feat: templates UI

--- src/components/cards/CardWithImageAndFooter.tsx
+++ src/components/cards/CardWithImageAndFooter.tsx
@@ -4,9 +4,25 @@ import style from './cardStyles.module.css';
 import { Eye } from 'react-bootstrap-icons';
 import { Col, OverlayTrigger, Row, Tooltip } from 'react-bootstrap';
 function CardWithImageAndFooter({ title, imgSrc }: { title: string; imgSrc: string }): JSX.Element {
+  const renderOverlay = () => {
+    return (
+      <div className={style.overlayContent}>
+        <Button variant="light" className={style.overlayButton}>
+          Select
+        </Button>
+        <Button variant="light" className={style.overlayButton}>
+          Info
+        </Button>
+      </div>
+    );
+  };
+
   return (
     <Card className={style.card}>
-      <Card.Img variant="top" src={imgSrc} />
+      <div className="position-relative">
+        {renderOverlay()}
+        <Card.Img variant="top" src={imgSrc} />
+      </div>
       <Card.Body>
         <Row>
           <Col xs="10">
@@ -15,17 +31,17 @@ function CardWithImageAndFooter({ title, imgSrc }: { title: string; imgSrc: stri
           <Col xs="1">
             <OverlayTrigger overlay={<Tooltip id={''}>Preview</Tooltip>}>
               <Button
-                variant="primary"
+                variant="dark"
                 style={{
-                  height: 28,
+                  height: 20,
                   borderRadius: 30,
-                  width: 30,
+                  width: 20,
                   display: 'flex',
                   justifyContent: 'center',
                   alignItems: 'center',
-                  padding: 0
+                  padding: 3
                 }}>
-                <Eye className="mt-3" />
+                <Eye className="mt-3" color="#fff" />
               </Button>
             </OverlayTrigger>
           </Col>
