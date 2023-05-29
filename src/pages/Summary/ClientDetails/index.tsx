6e95532df5e37aebf29af4d14c99a2ab3571eeac
Nikita Kumawat <35212192+Nikitakumawat@users.noreply.github.com>
2023-05-29 19:18:57 +0530

    Update index.tsx

--- src/pages/Summary/ClientDetails/index.tsx
+++ src/pages/Summary/ClientDetails/index.tsx
@@ -41,46 +41,6 @@ const ClientDetailsCard: React.FC<{
         ) : null}
     </Card>
 
-      // <Card className="border p-3 rounded mt-5">
-      //   <div className="d-flex">
-      //     <h4 className="col-lg-11 col-11">Client&apos;s details</h4>
-      //     <div
-      //       className="col-lg-11 col-sm-1"
-      //       onClick={() => setShowClientModal(true)}
-      //       style={{ cursor: 'pointer' }}>
-      //       <Icon name="edit" />
-      //     </div>
-      //   </div>
-      //   <table style={{ position: 'absolute', top: '50%' }}>
-      //     <tbody>
-      //       <tr>
-      //         <td>
-      //           <b className='mr-2'>Name </b>
-      //         </td>
-      //         <td>{`${Client[0].First_Name} ${Client[0].Last_Name}`}</td>
-      //       </tr>
-      //       <tr>
-      //         <td>
-      //           <b className='mr-2'>Phone no.</b>
-      //         </td>
-      //         <td>{Client[0].Phone_Number}</td>
-      //       </tr>
-      //       <tr>
-      //         <td>
-      //           <b className='mr-2'>Email</b>
-      //         </td>
-      //         <td>{Client[0].email}</td>
-      //       </tr>
-      //     </tbody>
-      //   </table>
-      //   {showClientModal ? (
-      //     <EditClient
-      //       show={showClientModal}
-      //       onHide={() => setShowClientModal(false)}
-      //       Client={Client}
-      //     />
-      //   ) : null}
-      // </Card>
   );
 };
 
