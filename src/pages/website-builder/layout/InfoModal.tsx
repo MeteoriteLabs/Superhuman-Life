e6a7bc041093eb89eea8c06d379f29a296e897dc
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2023-05-30 15:34:28 +0530

    feat: template-page

---
+++ src/pages/website-builder/layout/InfoModal.tsx
@@ -0,0 +1,95 @@
+import { WebsiteTemplate } from '../@types/websiteTemplates';
+import { ArrowRight, CheckCircle } from 'react-bootstrap-icons';
+import { Button, Col, ListGroup, Modal, Row } from 'react-bootstrap';
+
+import style from './style.module.css';
+
+function InfoModal({
+  data,
+  returnImageAlternating,
+  setInfoData
+}: {
+  data: WebsiteTemplate;
+  returnImageAlternating: (data: number) => string;
+  setInfoData: (data: WebsiteTemplate | null) => void;
+}): JSX.Element {
+  const handleClose = () => {
+    setInfoData(null);
+  };
+
+  return (
+    <Modal show={!!data} onHide={handleClose} dialogClassName="modal-80w">
+      <Modal.Body>
+        <Modal.Header closeButton className="mb-4">
+          <h3 className={style.templateName}>{data.attributes.template_name}</h3>
+        </Modal.Header>
+        <div className="position-relative">
+          <div
+            className="d-flex justify-content-center overflow-hidden position-relative"
+            style={{ borderRadius: '10px' }}>
+            <img
+              src={returnImageAlternating(Number(data.id))}
+              className={style.thumbnail}
+              width={500}
+            />
+          </div>
+          <Row className="mt-3">
+            <Col className="mt-3">
+              <h3 className={style.subheadings}>Good For</h3>
+
+              <ListGroup className="mt-3">
+                <ListGroup.Item>
+                  <CheckCircle className="text-success m-0 mr-2" />
+                  Coaches
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  {' '}
+                  <CheckCircle className="text-success m-0 mr-2" />
+                  Consultants
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  {' '}
+                  <CheckCircle className="text-success m-0 mr-2" />
+                  Athletes
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  {' '}
+                  <CheckCircle className="text-success m-0 mr-2" />
+                  Influencers
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col className="mt-3">
+              <h3 className={style.subheadings}>Features</h3>
+
+              <ListGroup className="mt-3">
+                <ListGroup.Item>
+                  {' '}
+                  <CheckCircle className="text-success m-0 mr-2" />
+                  Gallery Sections
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  {' '}
+                  <CheckCircle className="text-success m-0 mr-2" />
+                  Testimonials
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  {' '}
+                  <CheckCircle className="text-success m-0 mr-2" />
+                  Offerings
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+          </Row>
+        </div>
+        <div className="d-flex justify-content-end mt-5">
+          <Button variant="primary" className="py-2 px-4 " style={{}}>
+            Go to Live Editor <ArrowRight className="m-0 mb-1 ml-1" />
+          </Button>
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default InfoModal;
