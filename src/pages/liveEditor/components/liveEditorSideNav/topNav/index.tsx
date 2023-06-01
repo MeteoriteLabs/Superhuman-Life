14a8f63d8f64e05c97d63a29ba9df4d6bc7069fb
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2023-06-01 12:40:23 +0530

    feat: liveEditor-topNav

--- src/pages/liveEditor/components/liveEditorSideNav/topNav/index.tsx
+++ src/pages/liveEditor/components/liveEditorSideNav/topNav/index.tsx
@@ -8,23 +8,23 @@ function TopNav(): JSX.Element {
   return (
     <div className={style.topNav}>
       <Row className="mb-0">
-        <Col className="d-flex align-items-center" style={{ gap: '15px' }} xs="5">
-          <div style={{ marginBottom: '-15px' }}>
+        <Col className={style.topNavCol} lg="5" md="5" xs="4">
+          <div className={style.svgCont}>
             <ArrowRight width={10} height={8} />
           </div>
-          <p className="mb-0">Iron Man Template</p>
+          <p className="mb-0">Iron Man</p>
         </Col>
-        <Col className="d-flex align-items-center" style={{ gap: '10px' }} xs="5">
+        <Col className={style.topNavCol} lg="5" xs="4">
           <p className="mb-0">Options</p>
-          <div style={{ marginBottom: '-15px' }}>
+          <div className={style.svgCont}>
             <ArrowDown width={7} height={7} />
           </div>
         </Col>
-        <Col className="d-flex align-items-center" style={{ gap: '10px' }} xs="1">
-          <Button variant="outline-light" className="mb-0" style={{ fontSize: 12 }}>
+        <Col className={style.topNavCol} xs="1">
+          <Button variant="outline-light" className={style.btnCont}>
             Preview
           </Button>
-          <Button variant="primary" className="mb-0" style={{ fontSize: 12 }}>
+          <Button variant="primary" className={style.btnCont}>
             Publish
           </Button>
         </Col>
