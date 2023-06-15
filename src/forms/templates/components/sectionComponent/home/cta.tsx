46bfd3e88051784404f2a61ec5f12588578b18bc
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2023-06-15 10:13:57 +0530

    feat: hompage-sections-added

--- src/forms/templates/components/sectionComponent/home/cta.tsx
+++ src/forms/templates/components/sectionComponent/home/cta.tsx
@@ -1,7 +1,77 @@
-import style from './style.module.css';
+import { useForm, Controller } from 'react-hook-form';
+import style from '../style.module.css';
+import { Button, Form } from 'react-bootstrap';
 
-function Cta(): JSX.Element {
-  return <div>Cta</div>;
+type FormData = {
+  title: string;
+  buttonText: string;
+};
+
+function Hero(): JSX.Element {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors }
+  } = useForm<FormData>({
+    defaultValues: {
+      title: '',
+      buttonText: ''
+    }
+  });
+  const onSubmit = handleSubmit((data) => console.log(data));
+
+  return (
+    <div className={style.form}>
+      <Form onSubmit={onSubmit} style={{ width: 257 }}>
+        <Form.Group controlId="home_cta">
+          <Form.Label>Title</Form.Label>
+          <Controller
+            name="title"
+            control={control}
+            render={({ field }) => (
+              <Form.Control
+                type="text"
+                style={{ fontSize: 14 }}
+                as="input"
+                {...field}></Form.Control>
+            )}
+          />
+
+          {errors.title && <p>{errors.title.message}</p>}
+        </Form.Group>
+        <Form.Group controlId="home_cta">
+          <Form.Label>Button </Form.Label>
+          <Controller
+            name="buttonText"
+            control={control}
+            render={({ field }) => (
+              <Form.Control
+                type="textarea"
+                style={{ fontSize: 14 }}
+                as="input"
+                {...field}></Form.Control>
+            )}
+          />
+
+          {errors.buttonText && (
+            <Form.Control.Feedback tooltip>{errors.buttonText.message}</Form.Control.Feedback>
+          )}
+        </Form.Group>
+
+        <Button
+          variant="primary"
+          type="submit"
+          style={{
+            // paddingInline: 20,
+            paddingBlock: 4,
+            float: 'right',
+            marginBlock: 20
+          }}>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
-export default Cta;
+export default Hero;
