631c4ee2f730b9d0369157da02802351d0a4117d
Prakhar Gupta <prakharg658@gmail.com>
2023-06-16 22:43:49 +0530

    search bar on users page for contacts tab , ref: CT-70

--- src/builders/client-builder/leads/index.tsx
+++ src/builders/client-builder/leads/index.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState, useRef, useContext, useEffect } from "react";
+import { useMemo, useState, useRef, useContext, useEffect } from 'react';
 import {
   Badge,
   Button,
@@ -8,15 +8,15 @@ import {
   Card,
   Container,
   Row,
-  Col,
-} from "react-bootstrap";
-import Table from "../../../components/table/leads-table";
-import { useQuery, useMutation } from "@apollo/client";
-import AuthContext from "../../../context/auth-context";
-import ActionButton from "../../../components/actionbutton/index";
-import CreateEditMessage from "./createoredit-leads";
-import { GET_LEADS_NEW, UPDATE_SEEN_NEW } from "./queries";
-import { flattenObj } from "../../../components/utils/responseFlatten";
+  Col
+} from 'react-bootstrap';
+import Table from '../../../components/table/leads-table';
+import { useQuery, useMutation } from '@apollo/client';
+import AuthContext from '../../../context/auth-context';
+import ActionButton from '../../../components/actionbutton/index';
+import CreateEditMessage from './createoredit-leads';
+import { GET_LEADS_NEW, UPDATE_SEEN_NEW } from './queries';
+import { flattenObj } from '../../../components/utils/responseFlatten';
 
 export default function Leads() {
   const auth = useContext(AuthContext);
@@ -29,54 +29,51 @@ export default function Leads() {
   const [updateSeenStatus] = useMutation(UPDATE_SEEN_NEW, {
     onCompleted: (e: any) => {
       fetch.refetch();
-    },
+    }
   });
 
   const columns = useMemo<any>(
     () => [
-      { accessor: "leadsdate", Header: "Leads Date" },
-      { accessor: "name", Header: "Name" },
-      { accessor: "number", Header: "Number" },
-      { accessor: "email", Header: "Email" },
+      { accessor: 'leadsdate', Header: 'Leads Date' },
+      { accessor: 'name', Header: 'Name' },
+      { accessor: 'number', Header: 'Number' },
+      { accessor: 'email', Header: 'Email' },
       {
-        accessor: "isseen",
-        Header: "Is Read",
+        accessor: 'isseen',
+        Header: 'Is Read',
         Cell: (v: any) => (
           <Badge
             className="px-3 py-1"
-            style={{ fontSize: "1rem", borderRadius: "10px" }}
-            variant={v.value ? "success" : "danger"}
-          >
-            {v.value ? "Read" : "Unread"}
+            style={{ fontSize: '1rem', borderRadius: '10px' }}
+            variant={v.value ? 'success' : 'danger'}>
+            {v.value ? 'Read' : 'Unread'}
           </Badge>
-        ),
+        )
       },
       {
-        accessor: "source",
-        Header: "Source",
+        accessor: 'source',
+        Header: 'Source',
         Cell: (v: any) => (
           <Badge
             className="px-3 py-1"
-            style={{ fontSize: "1rem", borderRadius: "10px" }}
-            variant="secondary"
-          >
+            style={{ fontSize: '1rem', borderRadius: '10px' }}
+            variant="secondary">
             {v.value}
           </Badge>
-        ),
+        )
       },
-      { accessor: "lastupdated", Header: "Last Updated" },
+      { accessor: 'lastupdated', Header: 'Last Updated' },
       {
-        accessor: "status",
-        Header: "Status",
+        accessor: 'status',
+        Header: 'Status',
         Cell: (v: any) => (
           <Badge
             className="px-3 py-1"
-            style={{ fontSize: "1rem", borderRadius: "10px" }}
-            variant="secondary"
-          >
+            style={{ fontSize: '1rem', borderRadius: '10px' }}
+            variant="secondary">
             {v.value}
           </Badge>
-        ),
+        )
       },
 
       //    {
@@ -109,25 +106,25 @@ export default function Leads() {
       //         ),
       //    },
       {
-        id: "edit",
-        Header: "Actions",
+        id: 'edit',
+        Header: 'Actions',
         Cell: ({ row }: any) => {
           const editHandler = () => {
             createEditMessageComponent.current.TriggerForm({
               id: row.original.id,
-              type: "edit",
+              type: 'edit'
             });
           };
           const viewHandler = () => {
             createEditMessageComponent.current.TriggerForm({
               id: row.original.id,
-              type: "view",
+              type: 'view'
             });
           };
           const deleteHandler = () => {
             createEditMessageComponent.current.TriggerForm({
               id: row.original.id,
-              type: "delete",
+              type: 'delete'
             });
           };
 
@@ -135,8 +132,8 @@ export default function Leads() {
             updateSeenStatus({
               variables: {
                 seen: false,
-                id: row.original.id,
-              },
+                id: row.original.id
+              }
             });
           };
 
@@ -144,22 +141,22 @@ export default function Leads() {
             updateSeenStatus({
               variables: {
                 seen: true,
-                id: row.original.id,
-              },
+                id: row.original.id
+              }
             });
           };
 
           const arrayAction = [
-            { actionName: "Edit", actionClick: editHandler },
-            { actionName: "View", actionClick: viewHandler },
-            { actionName: "Mark as Unread", actionClick: markAsUnreadHandler },
-            { actionName: "Mark as Read", actionClick: markAsReadHandler },
-            { actionName: "Delete", actionClick: deleteHandler },
+            { actionName: 'Edit', actionClick: editHandler },
+            { actionName: 'View', actionClick: viewHandler },
+            { actionName: 'Mark as Unread', actionClick: markAsUnreadHandler },
+            { actionName: 'Mark as Read', actionClick: markAsReadHandler },
+            { actionName: 'Delete', actionClick: deleteHandler }
           ];
 
           return <ActionButton arrayAction={arrayAction}></ActionButton>;
-        },
-      },
+        }
+      }
     ],
     []
   );
@@ -177,7 +174,7 @@ export default function Leads() {
 
   const fetch = useQuery(GET_LEADS_NEW, {
     variables: { filter: searchFilter, id: auth.userid },
-    onCompleted: loadData,
+    onCompleted: loadData
   });
 
   function refetchQueryCallback() {
@@ -207,23 +204,22 @@ export default function Leads() {
           source: Detail.Details?.source,
           status: Detail.Details?.status,
           isseen: Detail?.isSeen,
-          lastupdated: getDate(Date.parse(Detail.updatedAt)),
+          lastupdated: getDate(Date.parse(Detail.updatedAt))
         };
       })
     );
     setNameArr(namearr);
   }
+
   useEffect(() => {
     if (searchFilter) {
       setDataTable(
         data.flatMap((Detail: any) => {
           if (
             (nameArr.includes(searchFilter) &&
-              Detail.Details?.leadsdetails?.name.toLowerCase() ===
-                searchFilter.toLowerCase()) ||
+              Detail.Details?.leadsdetails?.name.toLowerCase() === searchFilter.toLowerCase()) ||
             (nameArr.includes(searchFilter) &&
-              Detail.Details?.status.toLowerCase() ===
-                searchFilter.toLowerCase())
+              Detail.Details?.status.toLowerCase() === searchFilter.toLowerCase())
           ) {
             return {
               id: Detail.id,
@@ -233,7 +229,7 @@ export default function Leads() {
               email: Detail.Details?.leadsdetails.email,
               source: Detail.Details.source,
               status: Detail.Details.status,
-              lastupdated: getDate(Date.parse(Detail.updatedAt)),
+              lastupdated: getDate(Date.parse(Detail.updatedAt))
             };
           } else {
             return [];
@@ -241,7 +237,7 @@ export default function Leads() {
         })
       );
     }
-    if (searchFilter === "") {
+    if (searchFilter === '') {
       setDataTable(
         data.flatMap((Detail: any) => {
           return {
@@ -252,7 +248,7 @@ export default function Leads() {
             email: Detail.Details?.leadsdetails?.email,
             source: Detail.Details.source,
             status: Detail.Details.status,
-            lastupdated: getDate(Date.parse(Detail.updatedAt)),
+            lastupdated: getDate(Date.parse(Detail.updatedAt))
           };
         })
       );
@@ -265,19 +261,14 @@ export default function Leads() {
         <Row>
           <Col>
             <InputGroup className="mb-3">
-              <FormControl
-                aria-describedby="basic-addon1"
-                placeholder="Search"
-                ref={searchInput}
-              />
+              <FormControl aria-describedby="basic-addon1" placeholder="Search" ref={searchInput} />
               <InputGroup.Prepend>
                 <Button
                   variant="outline-secondary"
                   onClick={(e: any) => {
                     e.preventDefault();
                     setSearchFilter(searchInput.current.value);
-                  }}
-                >
+                  }}>
                   <i className="fas fa-search"></i>
                 </Button>
               </InputGroup.Prepend>
@@ -291,17 +282,15 @@ export default function Leads() {
                 onClick={() => {
                   createEditMessageComponent.current.TriggerForm({
                     id: null,
-                    type: "create",
-                    modal_status: true,
+                    type: 'create',
+                    modal_status: true
                   });
-                }}
-              >
+                }}>
                 <i className="fas fa-plus-circle"></i> Add Lead
               </Button>
               <CreateEditMessage
                 ref={createEditMessageComponent}
-                callback={refetchQueryCallback}
-              ></CreateEditMessage>
+                callback={refetchQueryCallback}></CreateEditMessage>
             </Card.Title>
           </Col>
         </Row>
