a1bb9a5cbe6449b6c6100ac96263734197564cf6
Nikita Kumawat <35212192+Nikitakumawat@users.noreply.github.com>
2023-06-19 16:27:43 +0530

    Update index.tsx

--- src/pages/liveEditor/components/liveEditorSideNav/index.tsx
+++ src/pages/liveEditor/components/liveEditorSideNav/index.tsx
@@ -1,9 +1,6 @@
 import { Nav } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-
-// import Tooltip from 'react-bootstrap/Tooltip';
-
 import style from './liveEditorSideNav.module.css';
 import CustomTabs from './tabs';
 
@@ -17,13 +14,6 @@ export default function LiveEditorSideNav({
   const location = useLocation();
   const [selectedOption, setSelectedOption] = useState<string>(location.pathname.slice(1));
   const [activeTab, setActiveTab] = useState<'website' | 'theme' | 'settings'>('website');
-
-  // * use when tooltip is needed
-  // const renderTooltip = (props) => (
-  //   <Tooltip id="button-tooltip" {...props}>
-  //     {props}
-  //   </Tooltip>
-  // );
 
   useEffect(() => {
     setSelectedOption(location.pathname.slice(1));
