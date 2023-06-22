a3289b725d7f8b2e78cfcd845b30b57c40538389
Prakhar Gupta <prakharg658@gmail.com>
2023-06-22 12:54:02 +0530

    fix: apply pagination on user page all three section , ref: CT-73

--- src/builders/client-builder/contacts/queries.ts
+++ src/builders/client-builder/contacts/queries.ts
@@ -1,12 +1,18 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 export const GET_CONTACTS = gql`
-  query ContactsQuery($id: ID){
-    contacts(pagination: { pageSize: 100 }, filters:{
-      ownedBy:{
-        id: {eq: $id}
+  query ContactsQuery($id: ID!,$filter: String!, $start: Int, $limit: Int) {
+    contacts(
+      pagination: { start: $start, limit: $limit }
+      filters: { ownedBy: { id: { eq: $id } } , firstname: { containsi: $filter } }
+      sort: ["updatedAt:desc"]
+    ) {
+      meta {
+        pagination {
+          pageCount
+          total
+        }
       }
-    } ) {
       data {
         id
         attributes {
@@ -16,8 +22,8 @@ export const GET_CONTACTS = gql`
           type
           phone
           createdAt
-          ownedBy{
-            data{
+          ownedBy {
+            data {
               id
             }
           }
@@ -32,9 +38,8 @@ export const GET_CONTACTS = gql`
 `;
 
 export const GET_CONTACT = gql`
-  query ContactQuery($id: ID){
-    contact(id: $id)
-    {
+  query ContactQuery($id: ID) {
+    contact(id: $id) {
       data {
         id
         attributes {
@@ -44,8 +49,8 @@ export const GET_CONTACT = gql`
           type
           phone
           createdAt
-          ownedBy{
-            data{
+          ownedBy {
+            data {
               id
             }
           }
@@ -60,11 +65,11 @@ export const GET_CONTACT = gql`
 `;
 
 export const GET_PAYMENT_SCHEDULES = gql`
-  query PaymentSchedulesQuery{
+  query PaymentSchedulesQuery {
     paymentSchedules(pagination: { pageSize: 100 }) {
-      data{
+      data {
         id
-        attributes{
+        attributes {
           Payment_Cycle
           PaymentCatagory
           Payment_DateTime
@@ -83,7 +88,7 @@ export const GET_PAYMENT_SCHEDULES = gql`
 
 export const ADD_CONTACT = gql`
   mutation contact($data: ContactInput!) {
-    createContact( data: $data ) {
+    createContact(data: $data) {
       data {
         id
         attributes {
@@ -93,8 +98,8 @@ export const ADD_CONTACT = gql`
           type
           phone
           createdAt
-          ownedBy{
-            data{
+          ownedBy {
+            data {
               id
             }
           }
@@ -110,7 +115,7 @@ export const ADD_CONTACT = gql`
 
 export const UPDATE_CONTACT = gql`
   mutation updateContact($id: ID!, $data: ContactInput!) {
-    updateContact(id: $id, data: $data ) {
+    updateContact(id: $id, data: $data) {
       data {
         id
       }
@@ -130,7 +135,7 @@ export const DELETE_CONTACT = gql`
 
 export const ADD_PAYMENT_SCHEDULE = gql`
   mutation createPaymentSchedule($data: PaymentScheduleInput!) {
-    createPaymentSchedule( data: $data ) {
+    createPaymentSchedule(data: $data) {
       data {
         id
       }
@@ -139,24 +144,24 @@ export const ADD_PAYMENT_SCHEDULE = gql`
 `;
 
 export const CREATE_NOTIFICATION = gql`
-     mutation createChangemakerNotification($data: ChangemakerNotificationInput!) {
-      createChangemakerNotification(data: $data) {
-        data{
-          id
-          attributes{
-            IsRead
-            type
-            Title
-            DateTime
-            Body
-            OnClickRoute
-            users_permissions_user{
-              data{
-                id
-              }
+  mutation createChangemakerNotification($data: ChangemakerNotificationInput!) {
+    createChangemakerNotification(data: $data) {
+      data {
+        id
+        attributes {
+          IsRead
+          type
+          Title
+          DateTime
+          Body
+          OnClickRoute
+          users_permissions_user {
+            data {
+              id
             }
           }
         }
       }
-     }
+    }
+  }
 `;
