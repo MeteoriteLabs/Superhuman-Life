001baab4a9a92bbf5469154baf4e858035b38762
Aksharmeet Singh <aksharmeetsingh21@gmail.com>
2023-06-22 08:27:47 +0530

    feat: refactor-quries-location

--- src/forms/templates/components/sectionComponent/home/quries/home.ts
+++ src/forms/templates/components/sectionComponent/home/quries/home.ts
@@ -1,26 +1,5 @@
 import { gql } from "@apollo/client";
 
-export const GET_WEBSITE_SECTION_ID = gql`
-query GetWebsiteSectionId($id: ID, $sectionPage: String, $sectionType: String) {
-  websiteSections(
-    filters: {
-      users_permissions_user: { id: { eq: $id } }
-      sectionPage: { eq: $sectionPage }
-      sectionType: { eq: $sectionType }
-    }
-  ) {
-    data {
-      id
-      attributes{
-        sectionData
-      }
-     
-  }
-}
-}
-`
-
-
 // * $sectionData structure
 //  {
 //     "image": "https://images.pexels.com/photos/3490348/pexels-photo-3490348.jpeg?auto=compress&cs=tinysrgb&w=800",
---
+++ src/forms/templates/components/sectionComponent/home/quries/index.ts
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+export const GET_WEBSITE_SECTION_ID = gql`
+query GetWebsiteSectionId($id: ID, $sectionPage: String, $sectionType: String) {
+  websiteSections(
+    filters: {
+      users_permissions_user: { id: { eq: $id } }
+      sectionPage: { eq: $sectionPage }
+      sectionType: { eq: $sectionType }
+    }
+  ) {
+    data {
+      id
+      attributes{
+        sectionData
+      }
+     
+  }
+}
+}
+`
