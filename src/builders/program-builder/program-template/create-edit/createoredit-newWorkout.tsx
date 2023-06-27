67ac54190949c7a2c0250206c5398ad12fd4298c
Nikita Kumawat <nikitakumawat44@gmail.com>
2023-06-27 10:17:40 +0530

    fix: change attribute name for changemaker availaibilty query, refs: #CT-107

--- src/builders/program-builder/program-template/create-edit/createoredit-newWorkout.tsx
+++ src/builders/program-builder/program-template/create-edit/createoredit-newWorkout.tsx
@@ -206,7 +206,7 @@ function CreateEditNewWorkout(props: any, ref: any) {
   async function updateSchedulerEvents(frm: any, workout_id: any) {
     const existingEvents = props.events === null ? [] : [...props.events];
    
-    if (frm.day) {
+    if (frm && frm.day) {
       frm.day = JSON.parse(frm.day);
     }
 
