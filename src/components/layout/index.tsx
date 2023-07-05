5ebd178acbc22b3d88fd5f8d0761ce376511c406
Prakhar Gupta <prakharg658@gmail.com>
2023-07-05 22:41:55 +0530

    fix:make sidenav responsive on small screen, ref:CT-105

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -43,7 +43,7 @@ export default function Layout({
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
-
+    
     return (
         <>
             <header>
--- src/pages/website-builder/index.tsx
+++ src/pages/website-builder/index.tsx
@@ -1,16 +1,182 @@
-import { FC, useState } from 'react';
+import { FC, useState, useContext, useEffect } from 'react';
 import SideNav from './layout/sidenav';
 import 'react-toggle/style.css';
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Row, Col, Nav , Navbar } from 'react-bootstrap';
 import WebsiteBuilder_settings from './components/websiteBuilder_settings';
 import WebsiteBuilder_template from './components/websiteBuilder_template';
+import { useLocation } from 'react-router-dom';
+import AuthContext from '../../context/auth-context';
+import Icon from '../../components/Icons';
+import MobileIcon from '../../components/Icons/mobile';
+import Icons from '../../components/Icons';
 
 const WebsiteBuilder: FC = () => {
     const [collapse, setCollapse] = useState<boolean>(true);
+    const [selected, setSelected] = useState<string>(location.pathname.slice(1));
+    const [sideNavStatus, setSideNavStatus] = useState<boolean>(false);
+    const { pathname } = useLocation();
+    const auth = useContext(AuthContext);
+    const [showSubOptionsWebsite, setShowSubOptionsWebsite] = useState(false);
+    const [showSubOptionsMobile, setShowSubOptionsMobile] = useState(false);
+
+    useEffect(() => {
+        getSideNavStatus();
+    }, [pathname]);
+
+    const getSideNavStatus = () => {
+        const currentSideNavStatus: boolean =
+            pathname !== '/lobby' &&
+            pathname !== '/website/templates/liveEditor' &&
+            pathname !== '/profile' &&
+            pathname !== '/insights' &&
+            pathname !== '/support'
+                ? true
+                : false;
+        setSideNavStatus(currentSideNavStatus);
+    };
 
     return (
         <>
-            <SideNav collapse={collapse} setCollapse={setCollapse} />
+            <Row noGutters className="bg-light">
+                <Col lg={collapse ? '1' : '2'} className="d-none d-lg-block">
+                    <SideNav collapse={collapse} setCollapse={setCollapse} />
+                </Col>
+            </Row>
+            {auth.token && sideNavStatus ? (
+                <Navbar
+                    expand="lg"
+                    fixed="bottom"
+                    bg="dark"
+                    variant="dark"
+                    className="d-lg-none d-sm-block mt-5"
+                >
+                    <div
+                        style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}
+                    >
+                        <Navbar>
+                            <Navbar.Brand
+                                onClick={() => {
+                                    setSelected('website');
+                                    setShowSubOptionsWebsite(!showSubOptionsWebsite);
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        textAlign: 'center',
+                                        
+                                    }}
+                                >
+                                    <Icon name="website" height={22} width={22} />
+                                </div>
+                                <small>Website</small>
+                            </Navbar.Brand>
+
+                            {selected === 'website' && showSubOptionsWebsite && (
+                                <Navbar.Collapse
+                                    id="basic-navbar-nav"
+                                    style={{ height: '20vh', width: '100vw' }}
+                                >
+                                    <Nav
+                                        className="mr-auto ml-3"
+                                        style={{ display: 'flex', flexDirection: 'column' }}
+                                    >
+                                        <Nav.Link
+                                            href="/theme-library"
+                                            style={{ color: 'white', display: 'flex' }}
+                                        >
+                                            <span className="ml-1">Theme Library</span>
+                                        </Nav.Link>
+                                        <Nav.Link
+                                            href="/live-editor"
+                                            style={{ color: 'white', display: 'flex' }}
+                                        >
+                                            <span className="ml-1">Live Editor</span>
+                                        </Nav.Link>
+                                    </Nav>
+                                </Navbar.Collapse>
+                            )}
+                        </Navbar>
+
+                        <Navbar>
+                            <Navbar.Brand
+                                onClick={() => {
+                                    setSelected('mobileApp');
+                                    setShowSubOptionsMobile(!showSubOptionsMobile);
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        textAlign: 'center',
+                                    }}
+                                >
+                                    <MobileIcon width={24} height={24} />
+                                </div>
+                                <small> Mobile Application</small>
+                            </Navbar.Brand>
+
+                            {selected === 'mobileApp' && showSubOptionsMobile && (
+                                <Navbar.Collapse
+                                    id="basic-navbar-nav"
+                                    style={{ height: '20vh', width: '100vw' }}
+                                >
+                                    <Nav
+                                        className="mr-auto ml-3"
+                                        style={{ display: 'flex', flexDirection: 'column' }}
+                                    >
+                                        <Nav.Link
+                                            href="#"
+                                            style={{ color: 'white', display: 'flex' }}
+                                        >
+                                            <span className="ml-1">Theme Library</span>
+                                        </Nav.Link>
+                                        <Nav.Link
+                                            href="/communication"
+                                            style={{ color: 'white', display: 'flex' }}
+                                        >
+                                            <span className="ml-1">Notification</span>
+                                        </Nav.Link>
+                                        <Nav.Link
+                                            href="/setting"
+                                            style={{ color: 'white', display: 'flex' }}
+                                        >
+                                            <span className="ml-1">Setting</span>
+                                        </Nav.Link>
+                                    </Nav>
+                                </Navbar.Collapse>
+                            )}
+                        </Navbar>
+                        <Navbar>
+                            <Navbar.Brand
+                                onClick={() => {
+                                    setSelected('');
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        textAlign: 'center'
+                                    }}
+                                >
+                                    <Icons width={22} height={22} name="linkicon" />
+                                </div>
+                                <small>Embeddable Links </small>
+                            </Navbar.Brand>
+                        </Navbar>
+                    </div>
+                </Navbar>
+            ) : null}
+
             <div className="d-flex">
                 {collapse ? (
                     <div style={{ width: '80px', height: '90vh', content: '' }} />
