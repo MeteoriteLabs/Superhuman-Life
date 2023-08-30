f5e1304ac5b5aeafaaf28e3cdc7b0ccab14061cf
Prakhar Gupta <prakharg658@gmail.com>
2023-08-30 13:35:04 +0530

    fix: remove insights and blog on lobby page, ref:CT-189

--- src/pages/dashboard/dashboard-data/data.tsx
+++ src/pages/dashboard/dashboard-data/data.tsx
@@ -1,19 +1,5 @@
 export const ImageCaptions = [
     {
-        image: 'assets/lobby_images/insights.svg',
-        color: '#A9A9A9',
-        link: '/insights',
-        imageWidth: '55px',
-        id: 1
-    },
-    {
-        image: 'assets/lobby_images/blog.svg',
-        color: '#B06AB3',
-        link: '/blog',
-        imageWidth: '40px',
-        id: 2
-    },
-    {
         image: 'assets/lobby_images/my-dashboard.svg',
         color: '#B73642',
         link: '/home',
