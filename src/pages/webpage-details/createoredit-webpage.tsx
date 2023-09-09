3a741e958e8c6f8f4e01d115c6773ed554f5d94b
Nikita Kumawat <35212192+Nikitakumawat@users.noreply.github.com>
2023-09-09 12:10:20 +0530

    Update createoredit-webpage.tsx

--- src/pages/webpage-details/createoredit-webpage.tsx
+++ src/pages/webpage-details/createoredit-webpage.tsx
@@ -178,6 +178,16 @@ const CreateWebpageDetails = (props: any, ref: any) => {
         });
     }
 
+     function ViewWebpage(frm: any) {
+        console.log('view message');
+    }
+
+    // eslint-disable-next-line
+    function DeleteWebpage(id: any) { 
+        console.log('delete message');
+        // deleteExercise({ variables: { id: id }});
+    }
+
     // eslint-disable-next-line
     function OnSubmit(frm: any) {
         //bind user id
